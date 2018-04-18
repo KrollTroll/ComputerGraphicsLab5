20617d52d9b5cffc5af0557a144c5bef5eff33e8
KrollTroll <krollcj@msoe.edu>
2018-04-18 14:34:49 -0500

    added sample code for basic line rubber-banding
    Signed-off-by: KrollTroll <krollcj@msoe.edu>

---
+++ src/drawing.cpp
@@ -0,0 +1,82 @@
+#include "drawing.h"
+#include "gcontext.h"
+// Constructor
+drawing::drawing()
+{
+dragging = false;
+x0 = x1 = y0 = y1 = 0;
+return;
+}
+void drawing::paint(GraphicsContext
+*
+gc)
+{
+// for fun, let’s draw a "fixed" shape in the middle of the screen
+// it will only show up after an exposure
+int middlex = gc->getWindowWidth()/2;
+int middley = gc->getWindowHeight()/2;
+gc->setColor(GraphicsContext::MAGENTA);
+for (int yi=middley-50;yi<=middley+50;yi++)
+{
+gc->drawLine(middlex-50,yi,middlex+50,yi);
+}
+gc->setColor(GraphicsContext::GREEN);
+// redraw the line if requested
+gc->drawLine(x0,y0,x1,y1);
+return;
+}
+void drawing::mouseButtonDown(GraphicsContext
+*
+gc, unsigned int button, int x, int y)
+{
+// mouse button pushed down
+// - clear context
+// - set origin of new line
+// - set XOR mode for rubber-banding
+// - draw new line in XOR mode.  Note, at this point, the line is
+//   degenerate (0 length), but need to do it for consistency
+x0 = x1 = x;
+y0 = y1 = y;
+gc->setMode(GraphicsContext::MODE_XOR);
+gc->drawLine(x0,y0,x1,y1);
+dragging = true;
+return;
+}
+void drawing::mouseButtonUp(GraphicsContext
+*
+gc, unsigned int button, int x, int y)
+{
+if(dragging)
+{
+// undraw old line
+gc->drawLine(x0,y0,x1,y1);
+// just in x and y here do not match x and y of last mouse move
+x1 = x;
+y1 = y;
+// go back to COPY mode
+gc->setMode(GraphicsContext::MODE_NORMAL);
+// new line drawn in copy mode
+gc->drawLine(x0,y0,x1,y1);
+// clear flag
+dragging = false;
+}
+return;
+}
+void drawing::mouseMove(GraphicsContext
+*
+gc, int x, int y)
+{
+if(dragging)
+{
+// mouse moved - "undraw" old line, then re-draw in new position
+// will already be in XOR mode if dragging
+// old line undrawn
+gc->drawLine(x0,y0,x1,y1);
+// update
+x1 = x;
+y1 = y;
+// new line drawn
+gc->drawLine(x0,y0,x1,y1);
+}
+return;
+}
---
+++ src/drawing.h
@@ -0,0 +1,35 @@
+#ifndef DRAWING_H
+#define DRAWING_H
+#include "drawbase.h"
+// forward reference
+class GraphicsContext;
+class drawing : public DrawingBase
+{
+public:
+drawing();
+// we will override just these
+virtual void paint(GraphicsContext
+*
+gc);
+virtual void mouseButtonDown(GraphicsContext
+*
+gc, unsigned int button,
+int x, int y);
+virtual void mouseButtonUp(GraphicsContext
+*
+gc, unsigned int button,
+int x, int y);
+virtual void mouseMove(GraphicsContext
+*
+gc, int x, int y);
+private:
+// We will only support one "remembered" line
+// In an actual implementation, we would also have one of our "image"
+// objects here to store all of our drawn shapes.
+int x0;
+int y0;
+int x1;
+int y1;
+bool dragging; // flag to know if we are dragging
+};
+#endif
